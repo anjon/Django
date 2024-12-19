80c289e66cb83233cdec889003597a6bf216686d
Anjon Sarker <anjon.sarker@gmail.com>
2024-12-19 12:11:51 +0000

    Update the 404 page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,8 @@
 function NotFound() {
-    return <div>Not Found</div>
+    return <div>
+        <h1>404 Not Found</h1>
+        <p>The page you're looking for doesn't exists!.</p>
+    </div>
 }
 
 export default NotFound
